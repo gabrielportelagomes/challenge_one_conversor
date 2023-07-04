d65ce447ce14602cee4a53ef9095e5f35a77903c
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-04 17:38:26 -0300

    feat: main class

---
+++ src/br/com/converter/Main.java
@@ -0,0 +1,12 @@
+package br.com.converter;
+
+import br.com.converter.menu.MainMenu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainMenu menu = new MainMenu();
+		menu.showMenu();
+	}
+
+}
